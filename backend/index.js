8361b9f97d1c08c5a101589373ea251341c831ec
Furkan YILDIRIM <furkanyildirim@yahoo.com>
2021-11-12 01:08:06 +0300

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app =express();
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
 
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose
-.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("MongoDB Connected!");
-})
-.catch((err) => console.log(err));
-app.use("/api/pins", pinRoute);
+mongoose 
+ .connect(process.env.MONGO_URL,
+  async(err)=>{
+    if(err) throw err;
+    console.log("MongoDB connected!")
+  }
+  )
+
 app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
-app.listen(8800,()=>{
-    console.log("Backend server is running!")
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
--- backend/models/Pin.js
+++ backend/models/Pin.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 
-const PinSchema = new mongoose.Schema({
+const PinSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
-    title:{
-        type: String,
-        require: true,
-        min: 3,
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 60,
     },
     desc: {
-        type: String,
-        require: true,
-        min: 3,
+      type: String,
+      required: true,
+      min: 3,
     },
     rating: {
-        type: Number,
-        require: true,
-        min: 0,
-        max: 5,
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
     },
-    lat:{
-        type: Number,
-        require: true,
+    long: {
+      type: Number,
+      required: true,
     },
-    long:{
-        type: Number,
-        require: true,
+    lat: {
+      type: Number,
+      required: true,
     },
- },
-{ timestamps: true }
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Pin", PinSchema);
